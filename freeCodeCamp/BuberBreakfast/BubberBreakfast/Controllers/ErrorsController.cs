3196afd169af371af18860b10f610b4be33cffe2
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-20 23:15:00 +0200

    Added Error handler

---
+++ freeCodeCamp/BuberBreakfast/BubberBreakfast/Controllers/ErrorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BubberBreakfast.Controllers
+{
+    public class ErrorsController : ControllerBase
+    {
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
