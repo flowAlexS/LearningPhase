2ca341729b25615c1255ef065c6dc535c14faa6a
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-20 21:22:13 +0200

    Ready the Api proj

--- freeCodeCamp/BuberBreakfast/BubberBreakfast/Program.cs
+++ freeCodeCamp/BuberBreakfast/BubberBreakfast/Program.cs
@@ -1,10 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
+{
+    builder.Services.AddControllers();
+}
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
