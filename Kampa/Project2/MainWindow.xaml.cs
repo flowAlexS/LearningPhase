1a392ac09847b6700d297d19270775fd4476be79
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-29 20:04:00 +0200

    Init

---
+++ Kampa/Project2/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Project2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
