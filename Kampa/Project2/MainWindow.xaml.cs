55bb31c709aff78d3eab77d10bd435419c52d9b7
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-29 20:28:42 +0200

    Finished proj 2

--- Kampa/Project2/MainWindow.xaml.cs
+++ Kampa/Project2/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace Project2;
+﻿using System.Windows;
+
+namespace Project2;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    bool running = false;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void BtnToggleRun_Click(object sender, RoutedEventArgs e)
+    {
+        running = !running;
+        tbStatus.Text = running ? "Running" : "Stopped";
+        btnToggleRun.Content = running ? "Stop" : "Run";
     }
 }
