50f7eb38898f43e1602a3d6287c4d1b299a17d66
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-14 11:47:10 +0300

    Impl Get File

--- PluralSight/CityInfo.API/CityInfo.API/Controllers/FilesController.cs
+++ PluralSight/CityInfo.API/CityInfo.API/Controllers/FilesController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace CityInfo.API.Controllers
 {
@@ -6,9 +9,32 @@ namespace CityInfo.API.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        => _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider 
+            ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+
+        [HttpGet("{fileId}")]
         public ActionResult GetFile(string fileId)
         {
-            throw new NotImplementedException();
+            // Classes to work with fiels -- FileContentResult, FileStreamResult
+            // PhysicaFileResult / VirtualFileResult
+
+            var pathToFile = "TestFile.docx";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream"; // Catch all
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
         }
     }
 }
