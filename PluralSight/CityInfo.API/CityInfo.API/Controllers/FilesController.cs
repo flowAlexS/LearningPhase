ff453b91c62345dd051217e4cf0cee49610d510a
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-14 11:25:31 +0300

    Init

---
+++ PluralSight/CityInfo.API/CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        public ActionResult GetFile(string fileId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
