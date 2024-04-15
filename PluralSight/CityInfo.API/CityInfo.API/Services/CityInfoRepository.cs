5d824a736100188e968f46d4824b8a7c4fc0336d
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-15 14:39:52 +0300

    Init

---
+++ PluralSight/CityInfo.API/CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,27 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        public Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<City?> GetCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest?> GetPointOfInterestForACity(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForACityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PluralSight/CityInfo.API/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,15 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+
+        Task<City?> GetCityAsync(int cityId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForACityAsync(int cityId);
+
+        Task<PointOfInterest?> GetPointOfInterestForACityAsync(int cityId, int pointOfInterestId);
+    }
+}
