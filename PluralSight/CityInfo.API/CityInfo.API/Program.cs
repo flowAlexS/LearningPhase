652952eabcf1d08409653c13ad32499fb20674cc
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-15 13:43:17 +0300

    Added Connection string

--- PluralSight/CityInfo.API/CityInfo.API/Program.cs
+++ PluralSight/CityInfo.API/CityInfo.API/Program.cs
@@ -47,8 +47,8 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 
 builder.Services.AddSingleton<CitiesDataStore>();
 
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => 
-    dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions =>
+    dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 var app = builder.Build();
 
