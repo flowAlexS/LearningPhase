1a0f32aea5f761c3bfe7dd4b541b566b3958477f
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-17 16:35:30 +0300

    Added move behaviour

--- PluralSight/WPF/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ PluralSight/WPF/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -20,5 +20,17 @@ namespace WiredBrainCoffee.CustomersApp
         {
             InitializeComponent();
         }
+
+        public void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            // Read Write attached property...
+            //var column =  (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var col = Grid.GetColumn(customerListGrid);
+            var newCol = col == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newCol);
+        }
     }
 }
