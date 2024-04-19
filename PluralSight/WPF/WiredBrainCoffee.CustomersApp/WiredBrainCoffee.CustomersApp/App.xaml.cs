c90ea7fac6faa5797089b3ab20897b6cb2306a41
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-19 13:35:24 +0300

    Added Dependency Injection

--- PluralSight/WPF/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ PluralSight/WPF/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,13 +1,39 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.InteropServices;
 using System.Windows;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomersViewModel>();
+            services.AddTransient<ProductsViewModel>();
+
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+        }
     }
 }
