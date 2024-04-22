f43e4534e4a74c8eee76569b265dd5987772d7ad
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-22 14:51:27 +0300

    Added endpoints testing

--- PluralSight/CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
+++ PluralSight/CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
@@ -48,17 +48,17 @@ namespace CityInfo.API.Controllers
         /// <summary>
         /// Get a city by id
         /// </summary>
-        /// <param name="id">The id of the city to get.</param>
+        /// <param name="cityId">The id of the city to get.</param>
         /// <param name="includePointsOfInterest">Whether or not to include the points of interest.</param>
         /// <returns>A city with or without points of interest.</returns>
         /// <response code="200">Returns the requested city.</response>
-        [HttpGet("{id}")]
+        [HttpGet("{cityId}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)] 
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult> GetCity(int id, bool includePointsOfInterest = false)
+        public async Task<ActionResult> GetCity(int cityId, bool includePointsOfInterest = false)
         {
-            var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
+            var city = await _cityInfoRepository.GetCityAsync(cityId, includePointsOfInterest);
             if (city is null)
             {
                 return NotFound();
