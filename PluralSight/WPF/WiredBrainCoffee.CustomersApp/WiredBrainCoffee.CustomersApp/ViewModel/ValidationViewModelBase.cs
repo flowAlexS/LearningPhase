d8dde933e6e7582c9480a4490f2c76daa6e8f242
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-04-22 16:51:55 +0300

    .

---
+++ PluralSight/WPF/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/ValidationViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new ();
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return propertyName is not null && _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName]
+                : Enumerable.Empty<string>();
+        }
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+        }
+    }
+}
